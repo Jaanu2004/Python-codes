b29cbb8271a8271d678db868f51e7fa565911ab3
Jaanu2004 <125555491+Jaanu2004@users.noreply.github.com>
2023-10-09 18:02:24 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,9 @@
+#arithmetic operations
+x=int(input("read x;"))
+y=int(input("read y;")) 
+print("Addition-", x+y)
+print("subtracyion-",x-y)
+print("Multiplication-",x*y)
+print("Division-", x/y)
+print("Division without decimal points+",x//y)
+print("Modular Division-",x%y)
