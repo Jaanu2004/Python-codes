667308f5e4317524aae568a26d29493177923575
Jaanu2004 <125555491+Jaanu2004@users.noreply.github.com>
2023-10-09 18:01:55 +0530

    Add files via upload

---
+++ ass-1(print statements).py
@@ -0,0 +1,7 @@
+print("My name is Jahnavi")
+print("My father name is Sreeram") 
+print("My mother name is Uma")
+print("My hobbies are watching movies and dancing")
+print("My intrests are knowing new things")
+print("I am studying Bsc data science 2nd year")
+print("My favorite color is black")
